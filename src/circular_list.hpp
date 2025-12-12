f7bc418e125ec98d14eb4896a27e883bc12df4ae
Huan Thieu Nguyen <nguyenthieuhuan@gmail.com>
2025-12-12 10:53:43 +0100

    circular list data structure

---
+++ src/circular_list.hpp
@@ -0,0 +1,35 @@
+#pragma once
+#include <stdint.h>
+
+template <typename T, auto Size> class CircularList {
+  struct Node {
+    T item = {};
+    Node *next;
+  };
+  Node list[Size];
+
+public:
+  consteval CircularList() {
+    list[Size - 1].next = &list[0];
+    for (auto i = 0; i < Size - 1; ++i) {
+      list[i].next = &list[i + 1];
+    }
+  }
+  [[nodiscard]] inline constexpr uint32_t length() { return Size; }
+  [[nodiscard]] auto inline constexpr head() -> Node & { return list[0]; }
+  [[nodiscard]] auto inline constexpr head() const -> const Node & {
+    return list[0];
+  }
+  [[nodiscard]] auto inline constexpr tail() -> Node & {
+    return list[Size - 1];
+  }
+  [[nodiscard]] auto inline constexpr tail() const -> const Node & {
+    return list[Size - 1];
+  }
+  auto inline constexpr operator[](uint32_t index) -> Node & {
+    return list[index % Size];
+  }
+  auto inline constexpr operator[](uint32_t index) const -> const Node & {
+    return list[index % Size];
+  }
+};
